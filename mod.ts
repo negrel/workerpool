b8eb1eeb4d05cbf4263f044612fb12de3f76aa61
Alexandre Negrel <alexandre@negrel.dev>
2024-05-27 00:07:50 +0200

    export worker script helper

--- mod.ts
+++ mod.ts
@@ -1,3 +1,9 @@
+/**
+ * This module implements a simple, extensible, type-safe and performant worker
+ * pool.
+ */
+
 export * from "./rpc.ts";
 export * from "./web_worker.ts";
 export * from "./worker_pool.ts";
+export * from "./worker.ts";
